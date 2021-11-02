969cbfaf58395380c12a03ebc4e20d938ae9f854
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-02 17:42:07 -0500

    fixed SELECTs on 2 and 9

--- joins-solution.sql
+++ joins-solution.sql
@@ -3,7 +3,7 @@ SELECT * FROM "customers"
 JOIN "addresses" ON "addresses"."customer_id" = "customers"."id";
 
 -- 2. Get all orders and their line items (orders, quantity and product).
-SELECT * FROM "orders"
+SELECT  "order_date", "description", "quantity" FROM "orders"
 JOIN "line_items" ON "line_items"."order_id" = "orders"."id"
 JOIN "products" ON "line_items"."product_id" = "products"."id";
 
@@ -38,10 +38,10 @@ WHERE "products"."description" = 'diet pepsi'
 GROUP BY "products"."description";
 
 -- 9. How much was the total cost for each order?
-SELECT "orders"."order_date", SUM("line_items"."quantity" * "products"."unit_price") FROM "orders"
+SELECT "orders"."id", SUM("line_items"."quantity" * "products"."unit_price") FROM "orders"
 JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
 JOIN "products" ON "products"."id" = "line_items"."product_id"
-GROUP BY "orders"."order_date"
+GROUP BY "orders"."id"
 
 -- 10. How much has each customer spent in total?
 
