095a9ac9d1d8a68770fee219ff4c9cbe1c2bf110
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-02 12:05:44 -0500

    9 complete

--- joins-solution.sql
+++ joins-solution.sql
@@ -37,4 +37,14 @@ JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
 WHERE "products"."description" = 'diet pepsi'
 GROUP BY "products"."description";
 
+-- 9. How much was the total cost for each order?
+SELECT "orders"."order_date", SUM("line_items"."quantity" * "products"."unit_price") FROM "orders"
+JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
+JOIN "products" ON "products"."id" = "line_items"."product_id"
+GROUP BY "orders"."order_date"
+
+-- 10. How much has each customer spent in total?
+
+
+-- 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
 
